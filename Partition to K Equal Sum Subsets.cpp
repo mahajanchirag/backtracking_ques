eb9ca89bca45fe10f881eca1bb6f8d655df22f98
mahajanchirag <70029905+mahajanchirag@users.noreply.github.com>
2021-07-16 02:17:30 +0530

    Update Partition to K Equal Sum Subsets.cpp

--- Partition to K Equal Sum Subsets.cpp
+++ Partition to K Equal Sum Subsets.cpp
@@ -1,85 +1,37 @@
 class Solution {
 public:
-     bool check(int i,vector<int> &visited,vector<int> &subset,vector<int> a,int si,int sum)  
-    {
-        if(visited[i]==1)
-        {
-            return false;
-        }
-        
-        if(subset[si]+a[i]>sum)
-        {
-            return false;
-        }
-        
-        return true;
-        
-    }
-  
-    bool solve(vector<int> a,vector<int> &subset,vector<int> &visited,int si,int n,int k,int sum)
-    {
-        if(subset[si]==sum)
-        {
-            if(si==k-2)
-            {
+    
+    bool dfs(vector<int>& nums, vector<int>& vis, int idx , int k, int curS, int tSum) {
+
+    if (k == 1) return true;
+    
+    if (curS == tSum) 
+        return dfs(nums, vis, 0, k-1, 0, tSum);
+    
+    for (int i=idx; i<nums.size(); i++) {
+        if (vis[i] == false && tSum >= curS+nums[i]) {
+            vis[i] = true;
+            if(dfs(nums, vis, i+1, k, curS+nums[i], tSum))
                 return true;
-            }
-            
-            return solve(a,subset,visited,si+1,n,k,sum);
+            vis[i] = false;
         }
-        
-        
-        for(int i=0;i<n;i++)
-        {
-            if(check(i,visited,subset,a,si,sum))
-            {
-                subset[si]=subset[si]+a[i];
-                visited[i]=1;
-                if(solve(a,subset,visited,si,n,k,sum))
-                {
-                    return true;
-                }
-                subset[si]=subset[si]-a[i];
-                visited[i]=0;
-            }
-        }
-        
-        return false;
+    }
+    return false;
+}
+    
+    
+    bool canPartitionKSubsets(vector<int>& nums, int k) 
+    {
+        int sum = 0;
+    for (int i=0; i<nums.size(); i++) { 
+        sum += nums[i];
     }
     
-    bool canPartitionKSubsets(vector<int>& a, int k) 
-    {
-       
-        int n=a.size();
-        if(n<k)
-         {
-             return false; 
-         }
-         
-         
-         if(k==1 )
-         {
-             return true;
-         }
-         
-         int sum=0;
-         for(int i=0;i<n;i++)
-         {
-             sum=sum+a[i];
-         }
-         
-         
-         if(sum%k!=0)
-         {
-             return false;
-         }
-         
-         sum=sum/k;
-         
-         vector<int> subset(k,0);
-         vector<int> visited(n,0);
-         
-         return solve(a,subset,visited,0,n,k,sum);
-         
+    if (sum % k != 0 || k > nums.size()) return false;
+    
+    int tarS = sum/k;
+    vector<int>vis(nums.size(), 0);
+    sort(nums.rbegin(), nums.rend());
+    return dfs(nums, vis, 0, k, 0, tarS);
     }
 };
